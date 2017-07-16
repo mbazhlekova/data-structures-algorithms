1518e4b42d6e4853c913b3f0de9ae92570307856
mbazhlekova <maria.bazhlekova@gmail.com>
2017-07-16 14:41:15 -0500

    Add Queue implementation

---
+++ Data Structures/Queue/queue.js
@@ -0,0 +1,28 @@
+/* JavaScript implementation of a Queue using an Array */
+
+function Queue () {
+  collection = [];
+  this.print= function() {
+    console.log(collection);
+  };
+
+  this.enqueue = function(element) {
+    collection.push(element);
+  };
+
+  this.dequeue = function() {
+    return collection.shift();
+  };
+
+  this.front = function() {
+    return collection[0];
+  };
+
+  this.size = function() {
+    return collection.length;
+  };
+
+  this.isEmpty = function() {
+    return (collection.length === 0);
+  };
+}
